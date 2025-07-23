53d451fa016197863c57c9e086403284bd4bae5a
WindowsMEMZ <memz1@darock.top>
2025-07-24 03:19:50 +0800

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Greatdori",
+    platforms: [.iOS(.v17), .macOS(.v14), .visionOS(.v1), .watchOS(.v10)],
+    products: [
+        .library(name: "DoriKit", targets: ["DoriKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.2"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.2"),
+        .package(url: "https://github.com/swift-library/swift-gyb", from: "0.0.1")
+    ],
+    targets: [
+        .target(
+            name: "DoriKit",
+            dependencies: [
+                "Alamofire",
+                "SwiftyJSON"
+            ],
+            path: "DoriKit/",
+            resources: [
+                .process("Localizable.xcstrings")
+            ],
+            plugins: [
+                .plugin(name: "Gyb", package: "swift-gyb")
+            ]
+        )
+    ]
+)
+
