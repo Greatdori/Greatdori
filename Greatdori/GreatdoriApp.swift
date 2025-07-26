bee2524726d032c7c3a27a7344796952be12ab33
WindowsMEMZ <memz1@darock.top>
2025-07-26 17:32:44 +0800

    Adds initial code for macOS/iOS target

--- Greatdori/GreatdoriApp.swift
+++ Greatdori/GreatdoriApp.swift
@@ -6,12 +6,64 @@
 //
 
 import SwiftUI
+import DoriKit
+import SDWebImage
+import SDWebImageSVGCoder
 
 @main
 struct GreatdoriApp: App {
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor var appDelegate: AppDelegate
+    #else
+    @UIApplicationDelegateAdaptor var appDelegate: AppDelegate
+    #endif
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+#if os(macOS)
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @Environment(\.locale) var locale
+    @AppStorage("IsFirstLaunch") var isFirstLaunch = true
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        SDImageCodersManager.shared.addCoder(SDImageSVGCoder.shared)
+        
+        if isFirstLaunch {
+            DoriAPI.preferredLocale = switch locale.language {
+            case let x where x.hasCommonParent(with: .init(identifier: "ja-JP")): .jp
+            case let x where x.hasCommonParent(with: .init(identifier: "en-US")): .en
+            case let x where x.isEquivalent(to: .init(identifier: "zh-TW")): .tw
+            case let x where x.hasCommonParent(with: .init(identifier: "zh-CN")): .cn
+            case let x where x.hasCommonParent(with: .init(identifier: "ko-KO")): .kr
+            default: .jp
+            }
+            isFirstLaunch = false
+        }
+    }
+}
+#else
+class AppDelegate: NSObject, UIApplicationDelegate {
+    @Environment(\.locale) var locale
+    @AppStorage("IsFirstLaunch") var isFirstLaunch = true
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        SDImageCodersManager.shared.addCoder(SDImageSVGCoder.shared)
+        
+        if isFirstLaunch {
+            DoriAPI.preferredLocale = switch locale.language {
+            case let x where x.hasCommonParent(with: .init(identifier: "ja-JP")): .jp
+            case let x where x.hasCommonParent(with: .init(identifier: "en-US")): .en
+            case let x where x.isEquivalent(to: .init(identifier: "zh-TW")): .tw
+            case let x where x.hasCommonParent(with: .init(identifier: "zh-CN")): .cn
+            case let x where x.hasCommonParent(with: .init(identifier: "ko-KO")): .kr
+            default: .jp
+            }
+            isFirstLaunch = false
+        }
+    }
+}
+#endif
