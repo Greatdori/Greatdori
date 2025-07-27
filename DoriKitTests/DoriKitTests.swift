9dc901b7fe69070b530cf32bc107b42da7689b64
WindowsMEMZ <memz1@darock.top>
2025-07-27 12:22:40 +0800

    Uses static icon for watchOS as a workarount to a known issue of App Store Connect

---
+++ DoriKitTests/DoriKitTests.swift
@@ -0,0 +1,16 @@
+//
+//  DoriKitTests.swift
+//  DoriKitTests
+//
+//  Created by Mark Chan on 7/27/25.
+//
+
+import Testing
+@testable import DoriKit
+
+struct DoriKitTests {
+    @Test
+    func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
