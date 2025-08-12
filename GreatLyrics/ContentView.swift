9f13bf5a1b12ac6c7f2b90e46d691d6ebb9f4d82
WindowsMEMZ <memz1@darock.top>
2025-08-13 00:38:17 +0800

    Create GreatLyrics

---
+++ GreatLyrics/ContentView.swift
@@ -0,0 +1,31 @@
+//===---*- Greatdori! -*---------------------------------------------------===//
+//
+// ContentView.swift
+//
+// This source file is part of the Greatdori! open source project
+//
+// Copyright (c) 2025 the Greatdori! project authors
+// Licensed under Apache License v2.0
+//
+// See https://greatdori.memz.top/LICENSE.txt for license information
+// See https://greatdori.memz.top/CONTRIBUTORS.txt for the list of Greatdori! project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreatLyrics/GreatLyricsApp.swift
@@ -0,0 +1,24 @@
+//===---*- Greatdori! -*---------------------------------------------------===//
+//
+// GreatLyricsApp.swift
+//
+// This source file is part of the Greatdori! open source project
+//
+// Copyright (c) 2025 the Greatdori! project authors
+// Licensed under Apache License v2.0
+//
+// See https://greatdori.memz.top/LICENSE.txt for license information
+// See https://greatdori.memz.top/CONTRIBUTORS.txt for the list of Greatdori! project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftUI
+
+@main
+struct GreatLyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
