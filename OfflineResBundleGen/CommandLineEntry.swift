c563e1230a1a7d5113e33d375095a48ee4c4fc75
WindowsMEMZ <memz1@darock.top>
2025-08-18 21:07:33 +0800

    Create OfflineResBundleGen target

---
+++ OfflineResBundleGen/CommandLineEntry.swift
@@ -0,0 +1,23 @@
+//===---*- Greatdori! -*---------------------------------------------------===//
+//
+// CommandLineEntry.swift
+//
+// This source file is part of the Greatdori! open source project
+//
+// Copyright (c) 2025 the Greatdori! project authors
+// Licensed under Apache License v2.0
+//
+// See https://greatdori.memz.top/LICENSE.txt for license information
+// See https://greatdori.memz.top/CONTRIBUTORS.txt for the list of Greatdori! project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct CommandLineEntry: AsyncParsableCommand {
+    mutating func run() async throws {
+        
+    }
+}
