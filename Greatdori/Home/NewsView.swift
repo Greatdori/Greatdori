160655622a8992803be22e9b38d6fb9f85683d05
ThreeManager785 <leimeichun50@gmail.com>
2025-08-19 18:46:29 +1000

    File structure update, search update (WIP)

---
+++ Greatdori/Home/NewsView.swift
@@ -0,0 +1,307 @@
+//===---*- Greatdori! -*---------------------------------------------------===//
+//
+// NewsView.swift
+//
+// This source file is part of the Greatdori! open source project
+//
+// Copyright (c) 2025 the Greatdori! project authors
+// Licensed under Apache License v2.0
+//
+// See https://greatdori.memz.top/LICENSE.txt for license information
+// See https://greatdori.memz.top/CONTRIBUTORS.txt for the list of Greatdori! project authors
+//
+//===----------------------------------------------------------------------===//
+
+import DoriKit
+import SwiftUI
+import SDWebImageSwiftUI
+
+//MARK: NewsView
+struct NewsView: View {
+    let filterLocalizedString: [DoriFrontend.News.ListFilter?: LocalizedStringResource] = [nil: "News.filter.selection.all", .bestdori: "News.filter.selection.bestdori", .article: "News.filter.selection.article", .patchNote: "News.filter.selection.patch-note", .update: "News.filter.selection.update", .locale(.jp): "News.filter.selection.jp", .locale(.en): "News.filter.selection.en", .locale(.tw): "News.filter.selection.tw", .locale(.cn): "News.filter.selection.cn", .locale(.kr): "News.filter.selection.kr"]
+    let filterOptions: [DoriFrontend.News.ListFilter?] = [nil, .bestdori, .article, .patchNote, .update, .locale(.jp), .locale(.en), .locale(.tw), .locale(.cn), .locale(.kr)]
+    @Environment(\.colorScheme) var colorScheme
+    @State var news: [DoriFrontend.News.ListItem]?
+    @State var filter: DoriFrontend.News.ListFilter? = nil
+    @State var allEvents: [DoriAPI.Event.PreviewEvent]?
+    @State var allGacha: [DoriAPI.Gacha.PreviewGacha]?
+    @State var allSongs: [DoriAPI.Song.PreviewSong]?
+    var dateFormatter = DateFormatter()
+    init() {
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+    }
+    var body: some View {
+        Group {
+            if let news {
+                List {
+                    ForEach(news, id: \.self) { news in
+                        NavigationLink(destination: {
+                            switch news.type {
+                            case .article:
+                                //FIXME: [NAVI785]
+                                EmptyView()
+                            case .event:
+                                EventDetailView(id: news.relatedID)
+                            case .gacha:
+                                //FIXME: [NAVI785]
+                                EmptyView()
+                            case .loginCampaign:
+                                //FIXME: [NAVI785]
+                                EmptyView()
+                            case .song:
+                                //FIXME: [NAVI785]
+                                EmptyView()
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }, label: {
+                            NewsPreview(allEvents: allEvents, allGacha: allGacha, allSongs: allSongs, news: news, showLocale: {
+                                if case .locale = filter { false } else { true }
+                            }(), showDetails: true, showImages: true)
+                        })
+//                        .navigationBarBackButtonHidden()
+//                        .navigationLinkIndicatorVisibility(.hidden)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(content: {
+                        Menu(content: {
+                            ForEach(0..<filterOptions.count, id: \.self) { filterIndex in
+                                Button(action: {
+                                    filter = filterOptions[filterIndex]
+                                }, label: {
+                                    HStack {
+                                        if filter == filterOptions[filterIndex] {
+                                            Image(systemName: "checkmark")
+                                        }
+                                        Text(filterLocalizedString[filterOptions[filterIndex]]!)
+                                    }
+                                })
+                            }
+                        }, label: {
+                            if filter != nil {
+                                Image(systemName: "line.3.horizontal.decrease")
+                                    .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                    .background {
+                                        Capsule().foregroundStyle(.blue).scaledToFill().scaleEffect(1.3)
+                                    }
+                            } else {
+                                Image(systemName: "line.3.horizontal.decrease")
+                            }
+                        })
+                    })
+                }
+            } else {
+                ProgressView()
+            }
+        }
+        .navigationTitle("Home.news")
+        .wrapIf(true, in: { content in
+            if filter != nil {
+                if #available(iOS 26.0, *) {
+                    content
+                        .navigationSubtitle(filterLocalizedString[filter]!)
+                } else {
+                    content
+                }
+            } else {
+                content
+            }
+        })
+        .task {
+            DoriCache.withCache(id: "News", trait: .realTime) {
+                await DoriFrontend.News.list(filter: filter)
+            } .onUpdate {
+                news = $0
+            }
+            await withTaskGroup { group in
+                group.addTask {
+                    let events = await DoriAPI.Event.all()
+                    await MainActor.run {
+                        allEvents = events
+                    }
+                }
+                group.addTask {
+                    let gacha = await DoriAPI.Gacha.all()
+                    await MainActor.run {
+                        allGacha = gacha
+                    }
+                }
+                group.addTask {
+                    let songs = await DoriAPI.Song.all()
+                    await MainActor.run {
+                        allSongs = songs
+                    }
+                }
+            }
+        }
+        .onChange(of: filter) {
+            Task {
+                news = await DoriFrontend.News.list(filter: filter)
+            }
+        }
+    }
+}
+
+//MARK: NewsPreview
+struct NewsPreview: View {
+    var allEvents: [DoriAPI.Event.PreviewEvent]?
+    var allGacha: [DoriAPI.Gacha.PreviewGacha]?
+    var allSongs: [DoriAPI.Song.PreviewSong]?
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @State var imageURL: URL? = nil
+    var news: DoriFrontend.News.ListItem
+    var showLocale: Bool = true
+    var showDetails: Bool = false
+    var showImages: Bool = false
+    var dateFormatter = DateFormatter()
+    init(
+        allEvents: [DoriAPI.Event.PreviewEvent]?,
+        allGacha: [DoriAPI.Gacha.PreviewGacha]?,
+        allSongs: [DoriAPI.Song.PreviewSong]?,
+        news: DoriFrontend.News.ListItem,
+        showLocale: Bool = true,
+        showDetails: Bool = false,
+        showImages: Bool = false
+    ) {
+        self.allEvents = allEvents
+        self.allGacha = allGacha
+        self.allSongs = allSongs
+        self.news = news
+        self.showLocale = showLocale
+        self.showDetails = showDetails
+        self.showImages = showImages
+        dateFormatter.setLocalizedDateFormatFromTemplate(showDetails ? "YMdjm" : "Mdjm")
+    }
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Group {
+                        Image(systemName: newsItemTypeIcon[news.type]!)
+                        Group {
+                            Text(newsItemTypeLocalizedString[news.type]!) + Text((showLocale && news.locale != nil) ? "Typography.brackets.with-space.\(news.locale!.rawValue.uppercased())" : "")
+                        }
+                        .offset(x: -3)
+                    }
+                    .foregroundStyle(newsItemTypeColor[news.type]!)
+                    Text(dateFormatter.string(from: news.timestamp))
+                        .foregroundStyle(.secondary)
+                }
+                .font(.footnote)
+                .bold()
+                //            .font(.caption)
+                Text(news.subject)
+                    .lineLimit(2)
+                    .foregroundStyle(.primary)
+                    .font(.body)
+                    .bold()
+                
+                Group {
+                    switch news.timeMark {
+                    case .willStartAfter(let interval):
+                        Text("News.time-mark.will-start-after.\(interval)")
+                    case .willEndAfter(let interval):
+                        Text("News.time-mark.will-end-after.\(interval)")
+                    case .willEndToday:
+                        Text("News.time-mark.will-end-today")
+                    case .hasEnded:
+                        Text("News.time-mark.has-ended")
+                    case .hasPublished:
+                        Text("News.time-mark.has-published")
+                    @unknown default:
+                        //                    Text("")
+                        EmptyView()
+                    }
+                }
+                .foregroundStyle(.primary)
+                .font(.body)
+                .fontWeight(.light)
+                
+                if showDetails {
+                    HStack {
+                        //                    Text("News.author.\(news.author)")
+                        Text(news.author)
+                            .font(.caption)
+                            .bold()
+                        ForEach(0..<news.tags.count, id: \.self) { tagIndex in
+                            Text("#\(news.tags[tagIndex])")
+                                .font(.caption)
+                            //                            .foregroundStyle(.secondary)
+                        }
+                        Spacer()
+                    }
+                    .foregroundStyle(.secondary)
+                }
+            }
+            Spacer()
+            
+            if showImages {
+                if sizeClass == .regular {
+                    Group {
+                        if let imageURL {
+                            WebImage(url: imageURL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: news.type == .song ? 100 : 75)
+                                .cornerRadius(5)
+                        } else {
+                            Rectangle()
+                                .frame(width: 0, height: 0)
+                                .opacity(0)
+                        }
+                    }
+                    Rectangle()
+                        .frame(width: 0, height: 0)
+                        .opacity(0)
+                        .wrapIf({ if case .event = news.type { true } else { false } }()) { content in
+                            content
+                                .onAppear {
+                                    if let allEvents {
+                                        imageURL = allEvents.first { $0.id == news.relatedID }?.logoImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                                .onChange(of: allEvents) {
+                                    if imageURL == nil, let allEvents {
+                                        imageURL = allEvents.first { $0.id == news.relatedID }?.logoImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                        }
+                        .wrapIf({ if case .gacha = news.type { true } else { false } }()) { content in
+                            content
+                                .onAppear {
+                                    if let allGacha {
+                                        imageURL = allGacha.first { $0.id == news.relatedID }?.logoImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                                .onChange(of: allEvents) {
+                                    if imageURL == nil, let allGacha {
+                                        imageURL = allGacha.first { $0.id == news.relatedID }?.logoImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                        }
+                        .wrapIf({ if case .song = news.type { true } else { false } }()) { content in
+                            content
+                                .onAppear {
+                                    if let allSongs {
+                                        imageURL = allSongs.first { $0.id == news.relatedID }?.jacketImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                                .onChange(of: allEvents) {
+                                    if imageURL == nil, let allSongs {
+                                        imageURL = allSongs.first { $0.id == news.relatedID }?.jacketImageURL(in: news.locale ?? .jp) ?? nil
+                                    }
+                                }
+                        }
+                }
+            }
+        }
+        
+    }
+}
+
+let newsItemTypeIcon: [DoriFrontend.News.ListItem.ItemType: String] = [.article: "text.page", .event: "star.hexagon", .gacha: "dice", .loginCampaign: "calendar", .song: "music.microphone"]
+let newsItemTypeColor: [DoriFrontend.News.ListItem.ItemType: Color] = [.article: .yellow, .event: .green, .gacha: .blue, .loginCampaign: .red, .song: .purple]
+let newsItemTypeLocalizedString: [DoriFrontend.News.ListItem.ItemType: LocalizedStringResource] = [.article: "News.type.article", .event: "News.type.event", .gacha: "News.type.gacha", .loginCampaign: "News.type.login-campaign", .song: "News.type.song"]
+//let newsTimeMarkTypeLocalizedString: [Dori]
---
+++ Greatdori/Home/SettingsView.swift
@@ -0,0 +1,321 @@
+//===---*- Greatdori! -*---------------------------------------------------===//
+//
+// SettingsView.swift
+//
+// This source file is part of the Greatdori! open source project
+//
+// Copyright (c) 2025 the Greatdori! project authors
+// Licensed under Apache License v2.0
+//
+// See https://greatdori.memz.top/LICENSE.txt for license information
+// See https://greatdori.memz.top/CONTRIBUTORS.txt for the list of Greatdori! project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftUI
+import DoriKit
+import WidgetKit
+
+struct SettingsView: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.horizontalSizeClass) var sizeClass
+    var usedAsSheet: Bool = false
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section(content: {
+                    SettingsServerView()
+                }, header: {
+                    Text("Settings.locale")
+                }, footer: {
+                    Text("Settings.birthday-time-zone.footer")
+                })
+                
+                Section(content: {
+                    SettingsHomeView()
+                }, header: {
+                    Text("Settings.home-edit")
+                }, footer: {
+                    Text(sizeClass == .compact ? "Settings.home-edit.footer.compact" : "Settings.home-edit.footer")
+                })
+                
+                #if os(iOS)
+                Section {
+                    SettingsWidgetView()
+                } header: {
+                    Text("Settings.widgets")
+                }
+                #endif
+                
+                #if DEBUG
+                Section(content: {
+                    SettingsDebugView()
+                }, header: {
+                    Text("Settings.debug")
+                })
+                #endif
+            }
+            .formStyle(.grouped)
+            .navigationTitle("Settings")
+            #if !os(macOS)
+            .wrapIf(usedAsSheet, in: { content in
+                content
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            DismissButton(action: dismiss.callAsFunction) {
+                                Image(systemName: "xmark")
+                            }
+                        }
+                    }
+            })
+            #endif
+        }
+    }
+}
+
+struct SettingsServerView: View {
+    @State var primaryLocale = "jp"
+    @State var secondaryLocale = "en"
+    @State var server: String = "jp"
+    @State var birthdayTimeZone: BirthdayTimeZone = .JST
+    var body: some View {
+        Group {
+            Picker(selection: $primaryLocale, content: {
+                Text("Home.servers.selection.jp")
+                    .tag("jp")
+                    .disabled(secondaryLocale == "jp")
+                Text("Home.servers.selection.en")
+                    .tag("en")
+                    .disabled(secondaryLocale == "en")
+                Text("Home.servers.selection.cn")
+                    .tag("cn")
+                    .disabled(secondaryLocale == "cn")
+                Text("Home.servers.selection.tw")
+                    .tag("tw")
+                    .disabled(secondaryLocale == "tw")
+                Text("Home.servers.selection.kr")
+                    .tag("kr")
+                    .disabled(secondaryLocale == "kr")
+            }, label: {
+                Text("Settings.servers.primaryLocale")
+            })
+            .onChange(of: primaryLocale, {
+                DoriAPI.preferredLocale = localeFromStringDict[primaryLocale] ?? .jp
+            })
+            Picker(selection: $secondaryLocale, content: {
+                Text("Home.servers.selection.jp")
+                    .tag("jp")
+                    .disabled(primaryLocale == "jp")
+                Text("Home.servers.selection.en")
+                    .tag("en")
+                    .disabled(primaryLocale == "en")
+                Text("Home.servers.selection.cn")
+                    .tag("cn")
+                    .disabled(primaryLocale == "cn")
+                Text("Home.servers.selection.tw")
+                    .tag("tw")
+                    .disabled(primaryLocale == "tw")
+                Text("Home.servers.selection.kr")
+                    .tag("kr")
+                    .disabled(primaryLocale == "kr")
+            }, label: {
+                Text("Settings.servers.secondaryLocale")
+            })
+            .onChange(of: secondaryLocale, {
+                DoriAPI.secondaryLocale = localeFromStringDict[secondaryLocale] ?? .en
+            })
+            Picker(selection: $birthdayTimeZone, content: {
+                Text("Settings.birthday-time-zone.selection.adaptive")
+                    .tag(BirthdayTimeZone.adaptive)
+                Text("Settings.birthday-time-zone.selection.JST")
+                    .tag(BirthdayTimeZone.JST)
+                Text("Settings.birthday-time-zone.selection.UTC")
+                    .tag(BirthdayTimeZone.UTC)
+                Text("Settings.birthday-time-zone.selection.CST")
+                    .tag(BirthdayTimeZone.CST)
+                Text(TimeZone(identifier: "America/Los_Angeles")!.isDaylightSavingTime() ? "Settings.birthday-time-zone.selection.PT.PDT" : "Settings.birthday-time-zone.selection.PT.PST")
+                    .tag(BirthdayTimeZone.PT)
+            }, label: {
+                Text("Settings.birthday-time-zone")
+            })
+            .onChange(of: birthdayTimeZone, {
+                UserDefaults.standard.setValue(birthdayTimeZone.rawValue, forKey: "BirthdayTimeZone")
+            })
+        }
+        .onAppear {
+            primaryLocale = localeToStringDict[DoriAPI.preferredLocale]?.lowercased() ?? "jp"
+            secondaryLocale = localeToStringDict[DoriAPI.secondaryLocale]?.lowercased() ?? "en"
+            birthdayTimeZone = BirthdayTimeZone(rawValue: UserDefaults.standard.string(forKey: "BirthdayTimeZone") ?? "JST") ?? .JST
+//            birthdayTimeZone = (UserDefaults.standard.value(forKey: "BirthdayTimeZone") ?? "JST")
+        }
+    }
+}
+
+let showBirthdayDateDefaultValue = 2
+struct SettingsHomeView: View {
+    @AppStorage("showBirthdayDate") var showBirthdayDate = showBirthdayDateDefaultValue
+    var body: some View {
+        HomeEditEventsPicker(id: 1)
+        HomeEditEventsPicker(id: 2)
+        HomeEditEventsPicker(id: 3)
+        HomeEditEventsPicker(id: 4)
+        Picker(selection: $showBirthdayDate, content: {
+            Text("Home.home.show-current-date.selection.always")
+                .tag(3)
+            Text("Home.home.show-current-date.selection.during-birthday")
+                .tag(2)
+            Text("Home.home.show-current-date.selection.automatic")
+                .tag(1)
+            Text("Home.home.show-current-date.selection.never")
+                .tag(0)
+        }, label: {
+            Text("Home.home.show-current-date")
+        })
+    }
+    
+    struct HomeEditEventsPicker: View {
+        @AppStorage("homeEventServer1") var homeEventServer1 = "jp"
+        @AppStorage("homeEventServer2") var homeEventServer2 = "cn"
+        @AppStorage("homeEventServer3") var homeEventServer3 = "tw"
+        @AppStorage("homeEventServer4") var homeEventServer4 = "en"
+        var id: Int = 1
+        init(id: Int = 1) {
+            self.id = id
+        }
+        var body: some View {
+            Picker(selection: (id == 1) ? $homeEventServer1 : ((id == 2) ? $homeEventServer2 : ((id == 3) ? $homeEventServer3 : $homeEventServer4)), content: {
+                Text("Home.servers.selection.jp")
+                    .tag("jp")
+                Text("Home.servers.selection.en")
+                    .tag("en")
+                Text("Home.servers.selection.cn")
+                    .tag("cn")
+                Text("Home.servers.selection.tw")
+                    .tag("tw")
+            }, label: {
+                Text("Home.servers.slot.\(id)")
+            })
+        }
+    }
+}
+
+#if os(iOS)
+struct SettingsWidgetView: View {
+    @State var cardIDInput = ""
+    var body: some View {
+        TextField("Settings.widget.ids", text: $cardIDInput)
+            .submitLabel(.done)
+            .onSubmit {
+                let ids = cardIDInput
+                    .replacingOccurrences(of: " ", with: "")
+                    .components(separatedBy: ",")
+                    .compactMap {
+                        if let direct = Int($0) {
+                            // "2125"
+                            return (id: direct, trained: false)
+                        } else if $0.contains(":") {
+                            // "1954:after"
+                            let separated = $0.components(separatedBy: ":")
+                            guard separated.count == 2 else { return nil }
+                            guard let id = Int(separated[0]) else { return nil }
+                            return (id: id, trained: separated[1] == "after")
+                        } else {
+                            return nil
+                        }
+                    }
+                Task {
+                    if let cards = await DoriAPI.Card.all() {
+                        let relatedCards = cards.compactMap { card in
+                            if ids.map({ $0.id }).contains(card.id) {
+                                (card: card, trained: ids.first(where: { $0.id == card.id })!.trained)
+                            } else {
+                                nil
+                            }
+                        }
+                        var descriptors = [CardWidgetDescriptor]()
+                        for (card, trained) in relatedCards {
+                            descriptors.append(
+                                .init(
+                                    cardID: card.id,
+                                    trained: trained,
+                                    localizedName: card.prefix.forPreferredLocale() ?? "",
+                                    imageURL: trained ? (card.coverAfterTrainingImageURL ?? card.coverNormalImageURL) : card.coverNormalImageURL
+                                )
+                            )
+                        }
+                        let encoder = PropertyListEncoder()
+                        encoder.outputFormat = .binary
+                        let containerPath = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.memz233.Greatdori.Widgets")!.path
+                        try? encoder.encode(descriptors).write(to: URL(filePath: containerPath + "/CardWidgetDescriptors.plist"))
+                        WidgetCenter.shared.reloadTimelines(ofKind: "com.memz233.Greatdori.Widgets.Card")
+                        print("Widget update succeeded")
+                    }
+                }
+            }
+            .task {
+                let containerPath = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.memz233.Greatdori.Widgets")!.path
+                let decoder = PropertyListDecoder()
+                if let data = try? Data(contentsOf: URL(filePath: containerPath + "/CardWidgetDescriptors.plist")),
+                   let descriptors = try? decoder.decode([CardWidgetDescriptor].self, from: data) {
+                    cardIDInput = descriptors.map { String($0.cardID) + ($0.trained ? ":after" : ":before") }.joined(separator: ", ")
+                }
+            }
+    }
+}
+#endif
+
+struct SettingsDebugView: View {
+    @AppStorage("debugShowHomeBirthdayDatePicker") var debugShowHomeBirthdayDatePicker = false
+    @AppStorage("isFirstLaunch") var isFirstLaunch = true
+    @AppStorage("isFirstLaunchResettable") var isFirstLaunchResettable = true
+    var body: some View {
+        Toggle(isOn: $debugShowHomeBirthdayDatePicker, label: {
+            Text(verbatim: "debugShowHomeBirthdayDatePicker")
+                .fontDesign(.monospaced)
+        })
+        Toggle(isOn: $isFirstLaunch, label: {
+            Text(verbatim: "isFirstLaunch")
+                .fontDesign(.monospaced)
+        })
+        Toggle(isOn: $isFirstLaunchResettable, label: {
+            Text(verbatim: "isFirstLaunchResettable")
+                .fontDesign(.monospaced)
+        })
+        #if !DORIKIT_ENABLE_PRECACHE
+        Text("Settings.debug.pre-cache-unavailable")
+            .foregroundStyle(.red)
+            .fontDesign(.monospaced)
+        #endif
+        NavigationLink(destination: {
+            DebugBirthdayView()
+        }, label: {
+            Text(verbatim: "DebugBirthdayView")
+                .fontDesign(.monospaced)
+        })
+    }
+}
+
+enum BirthdayTimeZone: String {
+    case adaptive
+    case JST
+    case UTC
+    case CST
+    case PT
+}
+
+
+func getBirthdayTimeZone() -> TimeZone {
+    switch BirthdayTimeZone(rawValue: UserDefaults.standard.string(forKey: "BirthdayTimeZone") ?? "JST")! {
+    case .adaptive:
+        return TimeZone.autoupdatingCurrent
+    case .JST:
+        return TimeZone(identifier: "Asia/Tokyo")!
+    case .UTC:
+        return TimeZone.gmt
+    case .CST:
+        return TimeZone(identifier: "Asia/Shanghai")!
+    case .PT:
+        return TimeZone(identifier: "America/Los_Angeles")!
+    }
+}
+
