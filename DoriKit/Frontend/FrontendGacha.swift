c318069e7edc02102fd02ce952a855bbfee5eace
Atlantic75 <113956386+ThreeManager785@users.noreply.github.com>
2025-09-13 16:40:43 +1000

    Update FrontendGacha.swift

--- DoriKit/Frontend/FrontendGacha.swift
+++ DoriKit/Frontend/FrontendGacha.swift
@@ -47,73 +47,7 @@ extension DoriFrontend {
             
             FilterCacheManager.shared.writeCardCache(cards)
             
-            var filteredGacha = gacha
-            if filter.isFiltered {
-                filteredGacha = gacha.filter {
-                    filter.gachaType.contains($0.type)
-                }.filter { gacha in
-                    if filter.attribute == Set(Filter.Attribute.allCases) {
-                        return true
-                    }
-                    let cards = cards.filter { gacha.newCards.contains($0.id) }
-                    return filter.attribute.contains { cards.map { $0.attribute }.contains($0) }
-                }.filter { gacha in
-                    if filter.character == Set(Filter.Character.allCases) {
-                        return true
-                    }
-                    let cards = cards.filter { gacha.newCards.contains($0.id) }
-                    if filter.characterRequiresMatchAll {
-                        return filter.character.allSatisfy { cards.map { $0.characterID }.contains($0.rawValue) }
-                    } else {
-                        return filter.character.contains { cards.map { $0.characterID }.contains($0.rawValue) }
-                    }
-                }.filter { gacha in
-                    for status in filter.released {
-                        for locale in filter.server {
-                            if status.boolValue {
-                                if (gacha.publishedAt.forLocale(locale) ?? dateOfYear2100) < .now {
-                                    return true
-                                }
-                            } else {
-                                if (gacha.publishedAt.forLocale(locale) ?? .init(timeIntervalSince1970: 0)) > .now {
-                                    return true
-                                }
-                            }
-                        }
-                    }
-                    return false
-                }.filter { gacha in
-                    for timelineStatus in filter.timelineStatus {
-                        let result = switch timelineStatus {
-                        case .ended:
-                            (gacha.closedAt.forPreferredLocale() ?? dateOfYear2100) < .now
-                        case .ongoing:
-                            (gacha.publishedAt.forPreferredLocale() ?? dateOfYear2100) < .now
-                            && (gacha.closedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)) > .now
-                        case .upcoming:
-                            (gacha.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)) > .now
-                        }
-                        if result {
-                            return true
-                        }
-                    }
-                    return false
-                }
-            }
-            
-            switch filter.sort.keyword {
-            case .releaseDate(let locale):
-                return filteredGacha.sorted { lhs, rhs in
-                    filter.sort.compare(
-                        lhs.publishedAt.forLocale(locale) ?? lhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0),
-                        rhs.publishedAt.forLocale(locale) ?? rhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)
-                    )
-                }
-            default:
-                return filteredGacha.sorted { lhs, rhs in
-                    filter.sort.compare(lhs.id, rhs.id)
-                }
-            }
+            return gacha
         }
         
         /// Get detailed gacha with related information.
