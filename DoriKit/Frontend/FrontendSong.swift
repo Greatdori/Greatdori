bf03179c00b009a9c1b1a5ece4b4715b4570fdd8
Atlantic75 <113956386+ThreeManager785@users.noreply.github.com>
2025-09-13 16:49:16 +1000

    Update FrontendSong.swift

--- DoriKit/Frontend/FrontendSong.swift
+++ DoriKit/Frontend/FrontendSong.swift
@@ -36,59 +36,7 @@ extension DoriFrontend {
         /// Other keys are ignored.
         public static func list(filter: Filter = .init()) async -> [PreviewSong]? {
             guard let songs = await DoriAPI.Song.all() else { return nil }
-            
-            var filteredSongs = songs
-            if filter.isFiltered {
-                filteredSongs = songs.filter {
-                    filter.songType.contains($0.tag)
-                }.filter {
-                    filter.band.map { $0.rawValue }.contains($0.bandID)
-                }.filter { song in
-                    for status in filter.released {
-                        for locale in filter.server {
-                            if status.boolValue {
-                                if (song.publishedAt.forLocale(locale) ?? dateOfYear2100) < .now {
-                                    return true
-                                }
-                            } else {
-                                if (song.publishedAt.forLocale(locale) ?? .init(timeIntervalSince1970: 0)) > .now {
-                                    return true
-                                }
-                            }
-                        }
-                    }
-                    return false
-                }.filter { song in
-                    for status in filter.released {
-                        for locale in filter.server {
-                            if status.boolValue {
-                                if (song.publishedAt.forLocale(locale) ?? dateOfYear2100) < .now {
-                                    return true
-                                }
-                            } else {
-                                if (song.publishedAt.forLocale(locale) ?? .init(timeIntervalSince1970: 0)) > .now {
-                                    return true
-                                }
-                            }
-                        }
-                    }
-                    return false
-                }
-            }
-            
-            switch filter.sort.keyword {
-            case .releaseDate(let locale):
-                return filteredSongs.sorted { lhs, rhs in
-                    filter.sort.compare(
-                        lhs.publishedAt.forLocale(locale) ?? lhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0),
-                        rhs.publishedAt.forLocale(locale) ?? rhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)
-                    )
-                }
-            default:
-                return filteredSongs.sorted { lhs, rhs in
-                    filter.sort.compare(lhs.id, rhs.id)
-                }
-            }
+            return songs
         }
         
         public static func allMeta(
