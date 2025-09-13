2f42acb4c3afa1edd4cd893208c2b1c7e5594568
Atlantic75 <113956386+ThreeManager785@users.noreply.github.com>
2025-09-13 16:44:04 +1000

    Update FrontendLoginCampaign.swift

--- DoriKit/Frontend/FrontendLoginCampaign.swift
+++ DoriKit/Frontend/FrontendLoginCampaign.swift
@@ -32,49 +32,9 @@ extension DoriFrontend {
         ///     - ``DoriFrontend/Filter/Sort/Keyword/id``
         ///
         /// Other keys are ignored.
-        public static func list(filter: Filter = .init()) async -> [PreviewCampaign]? {
+        public static func list() async -> [PreviewCampaign]? {
             guard let campaigns = await DoriAPI.LoginCampaign.all() else { return nil }
-            
-            var filteredCampaigns = campaigns
-            if filter.isFiltered {
-                filteredCampaigns = campaigns.filter { campaign in
-                    filter.loginCampaignType.contains { campaign.loginBonusType.rawValue == $0.rawValue }
-                }.filter { campaign in
-                    filter.server.contains { locale in
-                        campaign.publishedAt.availableInLocale(locale)
-                    }
-                }.filter { campaign in
-                    for timelineStatus in filter.timelineStatus {
-                        let result = switch timelineStatus {
-                        case .ended:
-                            (campaign.closedAt.forPreferredLocale() ?? dateOfYear2100) < .now
-                        case .ongoing:
-                            (campaign.publishedAt.forPreferredLocale() ?? dateOfYear2100) < .now
-                            && (campaign.closedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)) > .now
-                        case .upcoming:
-                            (campaign.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)) > .now
-                        }
-                        if result {
-                            return true
-                        }
-                    }
-                    return false
-                }
-            }
-            
-            switch filter.sort.keyword {
-            case .releaseDate(let locale):
-                return filteredCampaigns.sorted { lhs, rhs in
-                    filter.sort.compare(
-                        lhs.publishedAt.forLocale(locale) ?? lhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0),
-                        rhs.publishedAt.forLocale(locale) ?? rhs.publishedAt.forPreferredLocale() ?? .init(timeIntervalSince1970: 0)
-                    )
-                }
-            default:
-                return filteredCampaigns.sorted { lhs, rhs in
-                    filter.sort.compare(lhs.id, rhs.id)
-                }
-            }
+            return campaigns
         }
     }
 }
