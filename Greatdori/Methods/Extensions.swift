38ff3123268b7d670033d62b05502c1e7dbe973b
ThreeManager785 <leimeichun50@gmail.com>
2025-10-02 19:54:29 +1000

    Localization Update

--- Greatdori/Methods/Extensions.swift
+++ Greatdori/Methods/Extensions.swift
@@ -559,13 +559,13 @@ struct _ImageContextMenuModifier<V: View>: ViewModifier {
                     // Since `ShareLink` is not a button,
                     // we can't use `forEachImageInfo` here
                     if imageInfo.count > 1 {
-                        Menu("共享…", systemImage: "square.and.arrow.up") {
+                        Menu("Image.share", systemImage: "square.and.arrow.up") {
                             ForEach(imageInfo, id: \.self) { info in
                                 ShareLink(info.description ?? "Image.image", item: info.url)
                             }
                         }
                     } else if let info = imageInfo.first {
-                        ShareLink("共享…", item: info.url)
+                        ShareLink("Image.share", item: info.url)
                     }
                 }
                 if otherContentPlacement == .end, let otherContent {
