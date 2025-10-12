35bcd867033347b4dddebc201fafaeb197be84a3
ThreeManager785 <leimeichun50@gmail.com>
2025-10-12 11:53:44 +1000

    Option renaming

--- OfflineResBundleGen/CommandLineEntry.swift
+++ OfflineResBundleGen/CommandLineEntry.swift
@@ -50,7 +50,7 @@ struct CommandLineEntry: AsyncParsableCommand {
             await generateAPI(to: output)
         } else if locale == .doriResource {
             try await generateDoriResource(to: output)
-        } else if locale == .orbg2 {
+        } else if locale == .strandMain {
             await updateAssets(in: output, withToken: token)
         } else if locale == .debug {
             print("[$][DEBUG] Start Debug Process")
@@ -75,7 +75,7 @@ struct CommandLineEntry: AsyncParsableCommand {
         case kr
         case doriResource
         case debug
-        case orbg2
+        case strandMain
         
         var apiLocale: DoriAPI.Locale {
             switch self {
